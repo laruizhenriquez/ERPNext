8de723966732e0e3e86df62935e25ffdf56c7f36
Luis Ruiz <l.ruiz.henriquez@hotmail.cl>
2026-07-12 13:41:40 -0400

    feat(domain): add AuditableEntity

---
+++ src/ERPNext.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERPNext.Domain.Common;
+
+public abstract class AuditableEntity : BaseEntity
+{
+    public DateTime CreatedAt { get; protected set; }
+
+    public string? CreatedBy { get; protected set; }
+
+    public DateTime? UpdatedAt { get; protected set; }
+
+    public string? UpdatedBy { get; protected set; }
+
+    public DateTime? DeletedAt { get; protected set; }
+
+    public string? DeletedBy { get; protected set; }
+
+    public bool IsDeleted { get; protected set; }
+}
